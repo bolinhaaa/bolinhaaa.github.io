62ebe60015f4afe93ccef433f5a818120fb9259f
Bolinha <jonatabueno@live.com>
2020-02-27 17:18:06 -0300

    :tada: Primeiro Commit

---
+++ precache-manifest.aa4d2f2eb99f2a7bb56fee48c794c77d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ea5426fa47f5955014ff2a413339c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e25aabe642a83cf1f7a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "650043133e3018e9b9c6",
+    "url": "/static/css/main.a760b1c6.chunk.css"
+  },
+  {
+    "revision": "0e25aabe642a83cf1f7a",
+    "url": "/static/js/2.b9de9b03.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b9de9b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650043133e3018e9b9c6",
+    "url": "/static/js/main.b7250ef0.chunk.js"
+  },
+  {
+    "revision": "97607085263a23ec9a26",
+    "url": "/static/js/runtime-main.33bab405.js"
+  },
+  {
+    "revision": "f391e657f9cabe91f018ca14e340d693",
+    "url": "/static/media/logo.f391e657.svg"
+  }
+]);
